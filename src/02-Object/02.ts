c73052d163eca86a881de498e5b97e80127704dd
Aleksvtop <vazilino@gmail.com>
2024-02-07 22:09:46 +0300

    Created object car & type for it

--- src/02-Object/02.ts
+++ src/02-Object/02.ts
@@ -50,4 +50,80 @@ const student: StudentType = {
 
     ]
 }
+
+const student1: StudentType = {
+    id: 2,
+    name: "Victor",
+    age: 42,
+    isActive: true,
+    address: {
+        streetTitle: "Gromova 6",
+        city: {
+            title: "Minsk",
+            country: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "React"
+        },
+        {
+            id: 2,
+            title: "HTML"
+        },
+        {
+            id: 3,
+            title: "JS"
+        }
+    ]
+}
 console.log(student.technologies[1].title)
+console.log(student1.technologies[0].title)
+
+type CarType = {
+    mark: string,
+    class: string,
+    isNew: boolean,
+    regNumber: number,
+    regAddress: RegAddressType,
+    options: OptionsType[]
+}
+
+type RegAddressType = {
+    country: string,
+    city: string
+}
+
+type OptionsType = {
+    id: number,
+    value: string
+}
+
+const car: CarType = {
+    mark: "Audi",
+    class: "S",
+    isNew: true,
+    regNumber: 666546,
+    regAddress: {
+        country: "Belarus",
+        city: "Minsk"
+    },
+    options: [
+        {
+            id:1,
+            value: "4matic"
+        },
+        {
+            id:2,
+            value: "xenon"
+        },
+        {
+            id:3,
+            value: "loudMusic"
+        }
+    ]
+}
+
+console.log(car.isNew)
+console.log(car.options[2].value)
